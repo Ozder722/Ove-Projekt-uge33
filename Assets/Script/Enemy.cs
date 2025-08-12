6bfc4feebb7c922c4a78ad7fbf4be7893f01527c
Mikkelmig <mikkelhholm@gmail.com>
2025-08-12 22:05:39 +0200

    MVPGame

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y<-6f)
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+}
