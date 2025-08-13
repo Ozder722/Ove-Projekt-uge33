65ca949a3d1a23137cb175ac3ba3d3d333f7aa9f
Mikkelmig <mikkelhholm@gmail.com>
2025-08-13 13:29:10 +0200

    Background

--- Assets/Script/Scrolling.cs
+++ Assets/Script/Scrolling.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class Scrolling : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float speed;
+
+    [SerializeField]
+    private Renderer bgRenderer;
 
     // Update is called once per frame
     void Update()
     {
-        
+        bgRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0); 
     }
 }
