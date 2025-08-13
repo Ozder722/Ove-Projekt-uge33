2ec83b49c30381d08cd1726823f017edeae1fb07
Ozder722 <carlosozdk@gmail.com>
2025-08-13 14:00:57 +0200

    knapper

--- Assets/StartGame.cs
+++ Assets/StartGame.cs
@@ -2,10 +2,11 @@ using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    public void OnPointerClick(PointerEventData pointerEventData)
+    public void StartTheGame()
     {
         GameManager.instance.StartGame();
     }
